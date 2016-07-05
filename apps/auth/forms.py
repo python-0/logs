e5ac81141ac5b521c398577790a113a79a13d711
dinglong.li <li.dinglong@163.com>
2016-07-05 09:50:22 +0800

    switch tabs to space

--- apps/auth/forms.py
+++ apps/auth/forms.py
@@ -2,8 +2,9 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class LoginForm(Form):
-	username = StringField('username', validators=[DataRequired(), Length(1,64)] )
-	passwd = PasswordField('password', validators=[DataRequired(), Length(1,64)] )
-	remember_me = BooleanField('keep me login in')
-	submit = SubmitField('log in')
+    username = StringField('username', validators=[DataRequired(), Length(1, 64)])
+    passwd = PasswordField('password', validators=[DataRequired(), Length(1, 64)])
+    remember_me = BooleanField('keep me login in')
+    submit = SubmitField('log in')
